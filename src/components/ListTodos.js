5863401a300cec2bb191a6c0da7955ec95c29e85
radhika <radhika_rb@hotmail.com>
2023-01-24 15:55:57 +0530

    2nd step delete fn  done

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const ListTodos = ({ todos, deleteTodos }) => <ul>{todos.map((todo, index) =>
 	<li key={ index }>{todo}
+		<button
+			id={ index }
+			onClick={ () => deleteTodos(index) }
+		>Delete</button>
 	</li>)}</ul>;
 
 export default ListTodos;
