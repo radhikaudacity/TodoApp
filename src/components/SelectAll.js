62d90bb2e8030e3a80c280d6156a00ad18a514f2
radhika <radhika_rb@hotmail.com>
2023-01-27 10:28:30 +0530

    select all refactored

--- src/components/SelectAll.js
+++ src/components/SelectAll.js
@@ -1,26 +1,23 @@
-import { peek } from '@laufire/utils/debug';
 import React from 'react';
 
-const handleChange = (event, { state, setState }) => {
-	peek(event.target.checked);
+const handleChange = (event, { state, setState }) =>
 	setState({
 		...state, todos: state.todos.map((todo) =>
 			({ ...todo, checked: event.target.checked })),
 	});
-};
 
 const SelectAll = (context) => {
-	const { state } = context;
+	const { state: { todos }} = context;
 
 	return (
-		state.todos.length > 0
-			? <div>
+		todos.length > 0
+			&& <div>
 				<input
 					type="checkbox"
 					onChange={ (event) => handleChange(event, context) }
 				/>Select All Todos
 			</div>
-			: <div/>
+
 	);
 };
 
